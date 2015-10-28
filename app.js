d9406fd4ddd14c2f28ee91af372a34c38bf2959c
zhangzf <fangzhzh@gmail.com>
2015-10-28 13:13:39 +0000

    half

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 var sleep = require('./routes/sleep');
 var steps = require('./routes/steps');
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', routes);
+app.use('/', index);
 app.use('/steps',steps);
 app.use('/sleep', sleep);
 app.use('/users', users);
